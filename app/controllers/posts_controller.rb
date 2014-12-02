5149a86fabf961816c284686d42f1b9e5fe08319
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-12-02 17:57:56 -0600

    Forgot to swithc to instance variable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      render :json => post, :status => 200
+      render :json => @post, :status => 200
     else
       render_errors
     end
